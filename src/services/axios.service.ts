f55e667f09cc83f343f1962945646321a8b228aa
CalmDeveloper <demir445@gmail.com>
2022-05-06 00:52:13 +0300

    start

---
+++ src/services/axios.service.ts
@@ -0,0 +1,6 @@
+import axios, {AxiosResponse} from "axios";
+import {baseURL} from "../constants";
+
+const axiosService=axios.create({baseURL})
+export type Res<T>=Promise<AxiosResponse<T>>
+export {axiosService}
